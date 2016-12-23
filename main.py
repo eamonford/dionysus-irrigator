07336b251e480441874d9c82365a32102977ccf4
Eamon Ford <eamonford@gmail.com>
2016-12-22 20:04:42 -0800

    fix(logging): fixed logging syntax error

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from influxdb import InfluxDBClient
+from dao.RuleDataAccessor import RuleDataAccessor
+import paho.mqtt.client as mqtt
+import Config
+import logging
+
+ruleDao = None
+sensorDao = None
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to mosquitto with result code "+str(rc))
 
 def on_message(client, userdata, msg):
         messageDict = json.loads(str(msg.payload))
+        sensorId = messageDict['sensor_id']
+        rules = ruleDao.getBySensorId(sensorId)
+        for rule in rules:
+            rule['sensor'] = sensorDao.getById(sensorId)
+
+        print rules
 
 def main():
-
-    # connect to postgres to get client
+    pgConnection = Config.Configuration().getDatabaseConnection()
+    ruleDao = RuleDataAccessor(pgConnection)
+    sensorDao = SensorDataAccessor(pgConnection)
 
     mqttClient = mqtt.Client()
     mqttClient.on_connect = on_connect
